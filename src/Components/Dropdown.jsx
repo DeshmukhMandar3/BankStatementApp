d5358eef5009f5ff5c0db37cf49a8b9c0ae18c29
DeshmukhMandar3 <mandardeshmukh.mud@gmail.com>
2023-05-24 10:16:59 +0530

    added select component

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import Style from "../Styles/Dropdown.module.css";
+import { AiFillCaretDown } from "react-icons/ai";
 
 const Dropdown = ({ options, func }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isSelected, setIsSelected] = React.useState(false);
+  const [selected, setSelected] = React.useState(options[0]);
   return (
-    <select onChange={(e) => func(e.target.value)} className={Style.select}>
-      {options.map((el) => {
-        return (
-          <option key={el} value={el}>
-            {el}
-          </option>
-        );
-      })}
-    </select>
+    <div>
+      <div className={Style.selectTag} onClick={() => setIsOpen(!isOpen)}>
+        {isSelected ? `${selected}` : options[0]}
+        <span style={{ color: "gray" }}>
+          <AiFillCaretDown />
+        </span>
+      </div>
+
+      <ul
+        className={Style.list}
+        style={{
+          display: isOpen ? "block" : "none",
+          height: options.length === 12 ? "200px" : "100px",
+        }}
+      >
+        {options.map((el) => {
+          return (
+            <li
+              key={el}
+              onClick={() => {
+                setSelected(el);
+                setIsSelected(true);
+                setIsOpen(!isOpen);
+                func(el);
+              }}
+            >
+              {el}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
